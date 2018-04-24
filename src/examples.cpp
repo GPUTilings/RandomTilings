3eb886d4763caaec53ebafda5ec23e4c4bc9e90e
Ananth <asridhar@berkeley.edu>
2018-04-23 17:45:19 -0700

    Some minor fixes.

--- src/examples.cpp
+++ src/examples.cpp
@@ -14,11 +14,14 @@
 #include "./domino/DominoTiler.h"
 #include "./sixvertex/SixVertexTiler.h"
 
-void DominoBasicEx();
+void DominoBasicEx(); // A minimal example.
+void AztecDiamondCFTP(int N); // Tiles an Aztec Diamond of order N, using coupling from the past.
+
 
 int main() {
-	DominoBasicEx();
+	AztecDiamondCFTP(10);
 }
+
 
 void DominoBasicEx() {
 	std::cout<<"Running Domino, Basic Example."<<std::endl;
@@ -62,5 +65,57 @@ void DominoBasicEx() {
 }
 
 
+void AztecDiamondCFTP(int N) {
+	std::cout<<"Running Aztec Diamond CFTP."<<std::endl;
+
+	//Standard OpenCL set up code.
+	//PrintOpenCLInfo(); // Look at what devices are available
+	cl::Context context(CL_DEVICE_TYPE_DEFAULT);
+	std::string sinfo;
+	std::vector<cl::Device> devices;
+	context.getInfo(CL_CONTEXT_DEVICES, &devices);
+	devices[0].getInfo(CL_DEVICE_NAME, &sinfo); // Check which GPU you use!
+	std::cout<<"Created context using: "<<sinfo<<std::endl;
+	cl::CommandQueue queue(context);
+
+	cl_int err = 0;
+
+	domain d = DominoTiler::AztecDiamond(N);
+	tiling tMax = DominoTiler::MaxTiling(d);
+	tiling tMin = DominoTiler::MinTiling(d);
+
+	DominoTiler D(context, queue, devices, "./src/domino/dominokernelCFTP.cl", err); // use the CFTP kernel!
+	D.LoadTinyMT("./src/TinyMT/tinymt32dc.0.1048576.txt", tMax.size()/2);
+
+	std::mt19937 mt_rand(345903);
+
+
+	// Coupling from the past, with step sizes increasing by powers of two.
+	std::vector<long> steps; std::vector<long> seeds;
+
+	steps.push_back(128); seeds.push_back(mt_rand());
+
+
+	while ( true ) {
+		tiling top = tMax;
+		tiling bottom = tMin;
+
+		// run the random walks with the list of steps and seeds
+		D.Walk(bottom, steps, seeds);
+		D.Walk(top, steps, seeds);
+
+		// check if coupled
+		bool coupled = true;
+		for (int i = 0; i < top.size(); i++)
+			coupled = coupled & (top[i] == bottom[i]);
+
+		if ( coupled ) {
+			DominoTiler::TilingToSVG(top, "./examples/AztecDiamondCFTP/RandomTiling.svg");
+			break;
+		}
+
+		steps.insert(steps.begin(),steps[steps.size()-1]*2); seeds.insert(seeds.begin(),mt_rand());
+	}
+}
 
 
